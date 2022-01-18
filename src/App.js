76ae5ff797251d4d37ce072ebd4074aa74c28100
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-18 03:57:40 -0500

    Added the second state component, countries. Also added an Effect-hook

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { useState } from 'react'
 import Filter from './components/Filter'
 
 const App = () => {
+  const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
   
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+  console.log('render', countries.length, 'countries')
+
   const handleFilterChange = (event) => {
     console.log(`filter will be set to ${event.target.value}`)
     setFilter(event.target.value)
